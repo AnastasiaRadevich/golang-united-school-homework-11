40c5ffd09e15d72abdd86926c9cc771af1d8127d
ARadzevich <Anastasiya_Radzevich@epam.com>
2022-07-05 22:12:36 +0400

    feat: add homework-11

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,21 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var wg sync.WaitGroup
+	parallel := int(pool)
+
+	sem := make(chan struct{}, parallel)
+	res = make([]user, n)
+
+	for i := range res {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(j int) {
+			res[j] = getOne(int64(j))
+			<-sem
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return res
 }
